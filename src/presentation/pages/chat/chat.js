b37383ce7c3289df5210eddab3a92b00210a466c
farnazii <ffarzipour92@gmail.com>
2023-06-20 15:46:22 +0330

    chat message

--- src/presentation/pages/chat/chat.js
+++ src/presentation/pages/chat/chat.js
@@ -1,11 +1,13 @@
 import ChatHeader from "./components/chatHeader/chatHeader";
 import ChatContent from "./components/chatContent/chatContent";
+import ChatInput from "./components/chatInput/chatInput";
 import styles from "./chat.module.scss";
 function Chat() {
   return (
     <div className={styles.container}>
       <ChatHeader />
       <ChatContent />
+      <ChatInput />
     </div>
   );
 }
---
+++ src/presentation/pages/chat/components/chatInput/chatInput.js
@@ -0,0 +1,15 @@
+import BaseInput from "../../../../components/BaseInput/baseInput";
+import styles from "./chatInput.module.scss";
+function ChatInput({ message, handleMessage, sendMessage }) {
+  return (
+    <form className={styles.inputMessage} onSubmit={sendMessage}>
+      <BaseInput
+        value={message}
+        onChange={handleMessage}
+        placeholder="message..."
+      />
+    </form>
+  );
+}
+
+export default ChatInput;
